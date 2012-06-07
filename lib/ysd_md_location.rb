eebf98a4e001da8a41d4f6f52bf7e3cf7ccfdac0
yurak <yurak.sisa.dream@gmail.com>
2012-06-07 09:23:26 +0200

    first commit

---
+++ lib/ysd_md_location.rb
@@ -0,0 +1,26 @@
+require 'rubygems'
+require 'data_mapper'
+
+module LocationDataSystem
+
+  # ------------------------------------
+  # Represents a generic address 
+  # ------------------------------------
+  class Address
+     include DataMapper::Resource
+     
+     storage_names[:default] = 'locds_address' # stored in bookings table in default storage
+     
+     property :id, Serial, :field => 'id'
+     
+     property :street, String, :field => 'street', :length => 60
+     property :number, String, :field => 'number', :length => 10
+     property :complement, String, :field => 'complement', :length => 20
+     property :city, String, :field => 'city', :length => 60
+     property :state, String, :field => 'state', :length => 60
+     property :country, String, :field => 'country', :length => 50
+     property :zip, String, :field => 'zip', :length => 10
+  
+  end
+
+end
---
+++ ysd-md-location.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name    = "ysd_md_location"
+  s.version = "0.1"
+  s.authors = ["Yurak Sisa Dream"]
+  s.date    = "2012-03-06"
+  s.email   = ["yurak.sisa.dream@gmail.com"]
+  s.files   = Dir['lib/**/*.rb']
+  s.summary = "A DattaMapper-based model for booking"
+  
+  s.add_runtime_dependency "dm-core"
+end
